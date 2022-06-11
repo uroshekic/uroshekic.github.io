61c3dac6871554bf5bdc1f9322df3ed84f5f9795
Uroš Hekić <uros.hekic@marand.si>
2022-06-11 12:03:56 +0200

    Add static wp page

---
+++ wp/wp-login.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+	<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Lost Password &lsaquo; Uroš Hekić &#8212; WordPress</title>
+	<meta name='robots' content='noindex, nofollow, noarchive' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='http://localhost:8000/wp/wp-includes/css/dashicons.css?ver=5.9' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='http://localhost:8000/wp/wp-includes/css/buttons.css?ver=5.9' media='all' />
+<link rel='stylesheet' id='forms-css'  href='http://localhost:8000/wp/wp-admin/css/forms.css?ver=5.9' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='http://localhost:8000/wp/wp-admin/css/l10n.css?ver=5.9' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://localhost:8000/wp/wp-admin/css/login.css?ver=5.9' media='all' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login no-js login-action-lostpassword wp-core-ui  locale-en-us">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
+		<div id="login">
+		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	<p class="message">Please enter your username or email address. You will receive an email message with instructions on how to reset your password.</p>
+
+		<form name="lostpasswordform" id="lostpasswordform" action="http://localhost:8000/wp/wp-login.php?action=lostpassword" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="user_login" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
+						<input type="hidden" name="redirect_to" value="" />
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Get New Password" />
+			</p>
+		</form>
+
+		<p id="nav">
+			<a href="http://localhost:8000/wp/wp-login.php">Log in</a>
+					</p>
+				<p id="backtoblog">
+			<a href="http://localhost:8000/">&larr; Go to Uroš Hekić</a>		</p>
+			</div>
+				<div class="language-switcher">
+				<form id="language-switcher" action="" method="get">
+
+					<label for="language-switcher-locales">
+						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
+						<span class="screen-reader-text">Language</span>
+					</label>
+
+					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
+<option value="sl_SI" lang="sl" data-installed="1">Slovenščina</option></select>
+					
+					
+											<input type="hidden" name="action" value="lostpassword" />
+					
+						<input type="submit" class="button" value="Change">
+
+					</form>
+				</div>
+						<script type="text/javascript">
+		try{document.getElementById('user_login').focus();}catch(e){}
+		if(typeof wpOnload==='function')wpOnload();
+		</script>
+			<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+		<div class="clear"></div>
+	</body>
+	</html>
+	
